47de30812abda9d221a486c263375963b7016d3b
RidhoAnfaal <146906646+RidhoAnfaal@users.noreply.github.com>
2024-04-16 06:47:48 +0700

    jb2

---
+++ Jobsheet2/Buku21.java
@@ -0,0 +1,28 @@
+public class Buku21 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Halaman: " + halaman);
+        System.out.println("Stok: " + stok);
+        System.out.println("Harga: " + harga);
+    }
+
+    void terjual(int jml) {
+        if (stok > 0) {
+            stok -= jml;
+        } else {
+            System.out.println("Stok buku sudah habis");
+        }
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
